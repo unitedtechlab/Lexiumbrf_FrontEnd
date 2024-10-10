b63d3164c48d837403e03573614ff082f49554d8
khyati jethava <khyati_jethava@kainest.com>
2024-10-10 14:13:08 +0530

    workspace module

--- src/app/(pages)/workspace/page.tsx
+++ src/app/(pages)/workspace/page.tsx
@@ -3,7 +3,10 @@
 import React, { useState } from 'react';
 import styles from './workspace.module.css';
 import Searchbar from '@/app/components/Searchbar/search';
-import { Button } from 'antd';
+import { Button, Dropdown } from 'antd';
+import Image from "next/image";
+import User1 from '@/app/assets/images/user.jpg';
+import { HiOutlineDotsHorizontal } from "react-icons/hi";
 
 function Workspaces() {
     const [searchInput, setSearchInput] = useState("");
@@ -12,11 +15,113 @@ function Workspaces() {
         setSearchInput(event.target.value);
     };
 
+    const items = [
+        {
+            label: 'User Management',
+            key: 'user_manage',
+        },
+        {
+            label: 'Role Management',
+            key: 'role_manage',
+        },
+        {
+            label: 'Details',
+            key: 'details',
+        },
+        {
+            label: 'Delete',
+            key: 'delete',
+        },
+    ];
+
     return (
         <div className={styles.workspacePage}>
             <div className={`${styles.searchView} flex justify-space-between gap-1`}>
                 <Searchbar value={searchInput} onChange={handleSearchInputChange} />
                 <Button className="btn">Create</Button>
+            </div>
+
+            <div className={styles.workspaceWrapper}>
+                <div className={`flex gap-1 ${styles.workspaceBox}`}>
+                    <div className={styles.times}>
+                        <span>April, 08</span>
+                    </div>
+                    <div className={styles.nameList}>
+                        <div className={`flex gap-1 ${styles.dropdownList}`}>
+                            <h6>KaiNest Workspace</h6>
+                            <Dropdown menu={{ items }} trigger={['click']}>
+                                <Button
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                    }}
+                                    className={styles.btnDropdown}
+                                >
+                                    <HiOutlineDotsHorizontal />
+                                </Button>
+                            </Dropdown>
+                        </div>
+                        <div className={styles.usersImages}>
+                            <Image src={User1} alt="Users Image" width={26} height={26} loading="lazy" />
+                            <Image src={User1} alt="Users Image" width={26} height={26} loading="lazy" />
+                            <Image src={User1} alt="Users Image" width={26} height={26} loading="lazy" />
+                            <Image src={User1} alt="Users Image" width={26} height={26} loading="lazy" />
+                        </div>
+                    </div>
+                </div>
+
+                <div className={`flex gap-1 ${styles.workspaceBox}`}>
+                    <div className={styles.times}>
+                        <span>April, 08</span>
+                    </div>
+                    <div className={styles.nameList}>
+                        <div className={`flex gap-1 ${styles.dropdownList}`}>
+                            <h6>KaiNest Workspace</h6>
+                            <Dropdown menu={{ items }} trigger={['click']}>
+                                <Button
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                    }}
+                                    className={styles.btnDropdown}
+                                >
+                                    <HiOutlineDotsHorizontal />
+                                </Button>
+                            </Dropdown>
+                        </div>
+                        <div className={styles.usersImages}>
+                            <Image src={User1} alt="Users Image" width={26} height={26} loading="lazy" />
+                            <Image src={User1} alt="Users Image" width={26} height={26} loading="lazy" />
+                            <Image src={User1} alt="Users Image" width={26} height={26} loading="lazy" />
+                            <Image src={User1} alt="Users Image" width={26} height={26} loading="lazy" />
+                        </div>
+                    </div>
+                </div>
+
+                <div className={`flex gap-1 ${styles.workspaceBox}`}>
+                    <div className={styles.times}>
+                        <span>April, 08</span>
+                    </div>
+                    <div className={styles.nameList}>
+                        <div className={`flex gap-1 ${styles.dropdownList}`}>
+                            <h6>KaiNest Workspace</h6>
+                            <Dropdown menu={{ items }} trigger={['click']}>
+                                <Button
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                    }}
+                                    className={styles.btnDropdown}
+                                >
+                                    <HiOutlineDotsHorizontal />
+                                </Button>
+                            </Dropdown>
+                        </div>
+                        <div className={styles.usersImages}>
+                            <Image src={User1} alt="Users Image" width={26} height={26} loading="lazy" />
+                            <Image src={User1} alt="Users Image" width={26} height={26} loading="lazy" />
+                            <Image src={User1} alt="Users Image" width={26} height={26} loading="lazy" />
+                            <Image src={User1} alt="Users Image" width={26} height={26} loading="lazy" />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
