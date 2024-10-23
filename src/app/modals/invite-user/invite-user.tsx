f439c0dded8ede254e48cb6c484c933e1c2b23ca
AbhilashaGupta08 <abhilashag0801@gmail.com>
2024-10-23 20:05:13 +0530

    invited user side managed

--- src/app/modals/invite-user/invite-user.tsx
+++ src/app/modals/invite-user/invite-user.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { Modal, Button, message } from 'antd';
-import axios from 'axios';
-import { BaseURL } from '@/app/constants';
-import { getToken } from '@/utils/auth';
+import { Modal, Button } from 'antd';
 
 interface InviteModalProps {
     isModalVisible: boolean;
@@ -12,44 +9,16 @@ interface InviteModalProps {
 }
 
 const InviteModal: React.FC<InviteModalProps> = ({ isModalVisible, onClose, onAccept, onDecline }) => {
-    const handleApiCall = async (inviteResponse: boolean) => {
-        try {
-            const token = getToken();
-            if (!token) {
-                throw new Error('No token found, please login.');
-            }
-            const response = await axios.put(`${BaseURL}/enterprise_users`,
-                {
-                    inviteResponse: inviteResponse.toString(),
-                    accountID: 23
-                },
-                {
-                    headers: {
-                        "Authorization": `Bearer ${token}`
-                    }
-                }
-            );
-
-            if (response.status === 200 && response.data.success) {
-                message.success(inviteResponse ? 'Invite Accepted' : 'Invite Declined');
-                onClose();
-            } else {
-                message.error('Something went wrong. Please try again.');
-            }
-        } catch (error) {
-            message.error('Error occurred while processing your request.');
-        }
-    };
     return (
         <Modal
             title="You've been invited!"
             open={isModalVisible}
             onCancel={onClose}
             footer={[
-                <Button key="decline" onClick={() => handleApiCall(false)} danger>
+                <Button key="decline" onClick={onDecline} className="btn btn-delete">
                     Decline
                 </Button>,
-                <Button key="accept" onClick={() => handleApiCall(true)} type="primary">
+                <Button  className="btn" key="accept" onClick={onAccept} type="primary">
                     Accept
                 </Button>,
             ]}
